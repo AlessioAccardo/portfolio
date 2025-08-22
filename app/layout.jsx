4fe5e77934b9d75cd8dcf25bdb0ee2a71cb6db4b
AlessioAccardo <alessiohrdo@gmail.com>
2025-08-22 22:48:24 +0200

    about me

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ import { Raleway } from 'next/font/google';
 export const metadata = {
   title: "Alessio Accardo • Portfolio",
   description: "Alessio Accardo's Portfolio",
+  icons: {
+    icon: "/mio_logo.WEBP"
+  }
 };
 
 const raleway = Raleway({
@@ -19,7 +22,7 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body
-        className={`${raleway.className} text-white text-xl antialiased w-full max-w-full min-h-[1000vh] flex flex-col`}
+        className={`${raleway.className} text-white text-xl antialiased w-full max-w-full min-h-screen flex flex-col`}
       >
         <Header />
         <main className={`flex flex-col flex-1 inset-0 w-full max-w-full bg-slate-950 pointer-events-none`}>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,23 +3,49 @@ import Image from "next/image";
 function Hero() {
     return (
         <>
-            <div className="min-h-screen flex flex-col lg:flex-row justify-center items-center px-10 bg-slate-950">
-                <div className="flex flex-col justify-center items-center pb-5 text-center">
-                    <h1 className="text-5xl font-bold">ALESSIO ACCARDO</h1>
-                    <h3>AI/ML Engineer, Software Engineer & Web Developer</h3>
+            <div className="min-h-screen flex flex-col lg:flex-row justify-center items-center px-10 bg-slate-950 pt-10 gap-5 lg:gap-0">
+                <div className="flex flex-col justify-center items-center lg:pb-5 text-center">
+                    <h1 className="text-5xl font-bold textGradient">ALESSIO ACCARDO</h1>
+                    <h3 className="pt-3 font-medium">AI/ML Engineer, Software Engineer & Web Developer</h3>
                 </div>
                 <div>
                     <Image
                         src={"/img1.png"}
-                        width={600}
-                        height={600}
+                        width={500}
+                        height={500}
                         alt="hero image"
                     />
                 </div>
             </div>
-            <div className="min-h-screen flex flex-col items-start bg-gray-950">
-                <h1>PLACEHOLDER</h1>
-                <p>Placeholder of a placeholder</p>
+            <div className="min-h-screen flex flex-col items-center text-center font-medium bg-gray-950 py-10 lg:py-20">
+                <h1 className="uppercase text-5xl textGradient mb-10">ABOUT ME</h1>
+                <div className="flex flex-col lg:flex-row justify-center items-center lg:px-20 gap-10 lg:gap-0">
+                    <div className="flex flex-col w-full max-w-[70%] text-center lg:text-start gap-5">
+                        <p>
+                            I am currently a student in computer engineering bachelor's degree course.
+                        </p>
+                        <p>    
+                            I learned machine learning, programming in Python and Java, databases, internet protocols and some other amazing stuff!
+                        </p>
+                        <p>
+                            My main interests are the study of <span className="text-violet-500"><em>Python</em></span> and its application with <span className="text-violet-500"><em>PyTorch</em></span> and <span className="text-violet-500"><em>TensorFlow</em></span>.
+                        </p>
+                        <p>
+                            In addition to that I know SQL, MongoDB, Next.js, React, Angular, HTML5, CSS, Javascript and TypeScript.
+                        </p>
+                        <p>
+                            After my bachelor's degree, I will start the master's degree course in Computer Engineering: <span className="text-violet-500"><em>AI & Data Analysis</em></span>.
+                            </p>
+                    </div>
+                    <div className="relative w-full max-w-[30%]">
+                        <Image 
+                            src={"/hello.webp"}
+                            width={400}
+                            height={400}
+                            alt="hello"
+                        />
+                    </div>
+                </div>
             </div>
         </>
     );
